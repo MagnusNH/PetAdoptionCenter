a20db33b9453386c0c4e556241aee83e427fc7ce
Frederick <frdu0001@stud@ek.dk>
2025-09-15 14:11:33 +0200

    lizzard update

--- src/AdoptionCenter/Lizzard.java
+++ src/AdoptionCenter/Lizzard.java
@@ -1,6 +1,19 @@
 package AdoptionCenter;
 
 public class Lizzard extends Animals {
-    public lizzard();
-     super(name , )
+
+    public lizzard(String name, String animalType, String sound,);{
+     super(name,animalType,sound);
+
 }
+
+//getters
+public String getName() { return name; }
+public String getAnimalType() { return animalType; }
+public String getSound() {return sound; }
+
+//setters
+public String setName(){ return name; }
+public String setAnimalType(){ return animalType; }
+public String setSound() { return sound; }
+
