c9e14ecda80da62254839b67ab9dfbf934836d42
MagnusNH <Magn763j@stud.ek.dk>
2025-09-15 14:54:05 +0200

    Lizzzard

--- src/AdoptionCenter/AdoptionCenterTest.java
+++ src/AdoptionCenter/AdoptionCenterTest.java
@@ -5,23 +5,19 @@ import java.util.Scanner;
 public class AdoptionCenterTest {
     public void main (String[] args){
         Adoptioncenter();
-
+        int size=7;
     }
 
     public void Adoptioncenter(){
-        Scanner input = new Scanner(System.in);
-        Animals animals = new Animals("Navn:");
-
+        Scanner scan = new Scanner(System.in);
         Velkommen();
+        System.out.println("How many pets would you like to register?:");
+        String line1 = scan.nextLine();
     }
-
 
     public void Velkommen(){
         System.out.println("Welcome to the adoption center!");
         System.out.println("Lets register some animals!");
-        System.out.println("How many pets do you want to register?");
     }
-
-    Scanner input = new Scanner(System.in);
 
 }
--- src/AdoptionCenter/Animals.java
+++ src/AdoptionCenter/Animals.java
@@ -8,10 +8,11 @@ class Animals {
     protected String sound;
     private int [] location = {0,0,0,0,0,0};
 
-    public Animals (String Name, String animalType, String sound){
+    public Animals (String Name, String animalType, String sound, int location){
         this.name=Name;
         this.animalType=animalType;
         this.sound=sound;
+        this.location= new int[]{location};
     }
 
     Scanner scan=new Scanner (System.in);
