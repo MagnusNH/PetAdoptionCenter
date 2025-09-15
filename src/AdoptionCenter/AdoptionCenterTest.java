6c0ebb26d4f85f2e4478136026d99069a6024537
MagnusNH <Magn763j@stud.ek.dk>
2025-09-15 14:40:34 +0200

    Lizzzard

--- src/AdoptionCenter/AdoptionCenterTest.java
+++ src/AdoptionCenter/AdoptionCenterTest.java
@@ -1,4 +1,27 @@
 package AdoptionCenter;
 
+import java.util.Scanner;
+
 public class AdoptionCenterTest {
+    public void main (String[] args){
+        Adoptioncenter();
+
+    }
+
+    public void Adoptioncenter(){
+        Scanner input = new Scanner(System.in);
+        Animals animals = new Animals("Navn:");
+
+        Velkommen();
+    }
+
+
+    public void Velkommen(){
+        System.out.println("Welcome to the adoption center!");
+        System.out.println("Lets register some animals!");
+        System.out.println("How many pets do you want to register?");
+    }
+
+    Scanner input = new Scanner(System.in);
+
 }
--- src/AdoptionCenter/Animals.java
+++ src/AdoptionCenter/Animals.java
@@ -1,13 +1,18 @@
 package AdoptionCenter;
 
+import java.util.Scanner;
+
 class Animals {
     protected String name;
     protected String animalType;
     protected String sound;
+    private int [] location = {0,0,0,0,0,0};
 
     public Animals (String Name, String animalType, String sound){
         this.name=Name;
         this.animalType=animalType;
         this.sound=sound;
     }
+
+    Scanner scan=new Scanner (System.in);
 }
